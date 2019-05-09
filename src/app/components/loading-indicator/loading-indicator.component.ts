34556eb61fd4bfa487280362f45797c146509d74
Arto Österman <arto.osterman@dymesolutions.com>
2019-05-09 09:16:40 +0300

    Remove renaming input in LoadingIndicator

--- src/app/components/loading-indicator/loading-indicator.component.ts
+++ src/app/components/loading-indicator/loading-indicator.component.ts
@@ -7,7 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class LoadingIndicatorComponent implements OnInit {
 
-  @Input('showSpinner') showSpinner: boolean;
+  @Input() showSpinner: boolean;
 
   constructor() { }
 
